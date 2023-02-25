b6d73dee35526c0afa6ca83e52177a5dcdd1fee1
xiejiumi <xiejiumi@rt.com>
2023-02-25 10:26:06 +0800

    initial commit

--- 08_decorators/basic.py
+++ 08_decorators/basic.py
@@ -1,6 +1,22 @@
+import random
 import time
 
+# 无参数装饰器
+# def timer(func):
+#     def decorated(*args, **kwargs):
+#         st = time.perf_counter()
+#         ret = func(*args, **kwargs)
+#         print("time cost: {} seconds".format(time.perf_counter() - st))
+#         return ret
+#
+#     return decorated
+#
+# @timer
+# def random_sleep():
+#     time.sleep(random.random())
 
+
+# 有参数装饰器
 def timer(print_args=False):
     """装饰器：打印函数耗时
 
@@ -19,3 +35,12 @@ def timer(print_args=False):
         return wrapper
 
     return decorator
+
+
+@timer(print_args=True)
+def random_sleep():
+    time.sleep(random.random())
+
+
+if __name__ == '__main__':
+    random_sleep()
