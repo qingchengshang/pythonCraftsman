20719a1840df134724bbaf92016e6e5d6fd82ae1
xiejiumi <xiejiumi@rt.com>
2023-02-25 11:55:14 +0800

    initial commit

--- 08_decorators/class_as_deco.py
+++ 08_decorators/class_as_deco.py
@@ -2,7 +2,45 @@
 import time
 from functools import update_wrapper
 
+# import random
+# import time
+# from functools import wraps
+# from typing import Optional
+#
+#
+# 用类来实现装饰器（函数替换）
+# class timer:
+#     """装饰器：打印函数耗时
+#
+#     :param print_args: 是否打印被方法名和参数，默认为 False
+#     """
+#     def __init__(self, print_args: Optional = False):
+#         self.print_args = print_args
+#
+#     def __call__(self, func):
+#         @wraps(func)
+#         def decorated(*args, **kwargs):
+#             st = time.perf_counter()
+#             ret = func(*args, **kwargs)
+#             if self.print_args:
+#                 print(f'"{func.__name__}", args: {args}, kwargs: {kwargs}')
+#             print("time cost: {} seconds".format(time.perf_counter() - st))
+#             return ret
+#
+#         return decorated
+#
+#
+# @timer(print_args=True)
+# def random_sleep():
+#     """随机睡眠一小会儿"""
+#     time.sleep(random.random())
+#
+#
+# if __name__ == '__main__':
+#     random_sleep()
 
+
+# 用类来实现装饰器（示例替换）
 class DelayedStart:
     """在执行被装饰函数前，等待 1 秒钟"""
 
