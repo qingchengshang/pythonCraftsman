410c206ca1b44dbbd63a5a760609fd9275783c42
ChangYuan <1205326823@qq.com>
2023-02-26 09:34:18 +0800

    initial commit

--- 08_decorators/class_decorator.py
+++ 08_decorators/class_decorator.py
@@ -6,8 +6,8 @@ _validators = {}
 class ValidatorMeta(type):
     """元类：将所有校验器类统一注册起来，方便后续使用"""
 
-    def __new__(cls, name, bases, attrs):
-        ret = super().__new__(cls, name, bases, attrs)
+    def __new__(mcs, name, bases, attrs):
+        ret = super().__new__(mcs, name, bases, attrs)
         _validators[attrs["name"]] = ret
         return ret
 
