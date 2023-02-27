9321c88e9577b5608ee1038065efd182fc603c86
xiejiumi <xiejiumi@rt.com>
2023-02-27 18:59:21 +0800

    initial commit

--- 09_oop/mro.py
+++ 09_oop/mro.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
+# MRO: Method Resolution Order, 方法解析顺序. 解决：多重继承时，子类应该继承哪一个父类的方法
 class A:
     def say(self):
         print("I'm A")
@@ -17,3 +18,9 @@ class C(A):
 
 class D(B, C):
     pass
+
+
+if __name__ == '__main__':
+    d = D()
+    d.say()
+    print(D.__mro__)
