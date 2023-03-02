9ababf266d500bb97499e11959630d6b3133c069
xiejiumi <xiejiumi@rt.com>
2023-03-02 19:50:52 +0800

    initial commit

--- 09_oop/func_oop.py
+++ 09_oop/func_oop.py
@@ -20,6 +20,11 @@ class AppConfig:
         ...
 
 
+"""
+# 预绑定方法模式是一种将对象方法绑定为函数的模式。要实现该模式，只需要一个单例对象----模块(module)
+# 当你在python中执行import语句导入模块时，无论import执行了多少次，每个被导入的模块在内存中只会
+# 存在一份(保存在sys.modules中)。因此要实现单例模式，只需在模块里创建一个全局对象即可：
+"""
 class AppConfig:
     """程序配置类，使用单例模式"""
 
@@ -34,3 +39,17 @@ class AppConfig:
     def reload(self):
         """重新读取配置文件，刷新配置"""
         ...
+
+
+# 下一步，为了给其它模块提供好用的API，我们需要将单例对象_config的公有方法绑定到config模块上：
+# file: project/config.py
+_config = AppConfig()
+get_database_conf = _config.get_database
+reload_config = _config.reload
+
+"""
+# 之后，其它模块就可以像调用普通函数一样操作应用配置对象了：
+from project.config import get_database_conf, reload_config 
+db_conf = get_database_conf()
+reload_config()
+"""
