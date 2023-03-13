9fa0948d6ddb07b184d39ee01488088c208f8c53
xiejiumi <xiejiumi@rt.com>
2023-03-13 19:07:51 +0800

    initial commit

--- 09_oop/mro.py
+++ 09_oop/mro.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+""" MRO: Method Resolution Order, 方法解析顺序. 解决：多重继承时，子类应该继承哪一个父类的方法 """
 
 
-# MRO: Method Resolution Order, 方法解析顺序. 解决：多重继承时，子类应该继承哪一个父类的方法
 class A:
     def say(self):
         print("I'm A")
